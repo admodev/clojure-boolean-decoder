c2b16fcdb170a537a6e4acdbcd3f28630a8dba1b
admodev <adolfomoyano@exegesisba.com>
2022-02-22 13:48:39 -0300

    minor corrections to conditional statements

--- src/boolean_decoder/core.clj
+++ src/boolean_decoder/core.clj
@@ -4,9 +4,14 @@
 (defn decode []
   (println "Hello, Welcome to the boolean decoder!")
   (println "Please, enter the value to decode:")
-  (let [encoded-val (Integer/parseInt (read-line))]
-    (if (= 1 encoded-val)
-      (println true))
+  (let [encoded-val (read-line)]
+    (cond
+      (= "1" encoded-val) (println true)
+      (= "0" encoded-val) (println false)
+      (= "" encoded-val) (println "Please, enter a number between (1 or 0).")
+      (not= Integer encoded-val) (println "Please, enter a number...")
+      :default ""
+      )
     )
   )
 

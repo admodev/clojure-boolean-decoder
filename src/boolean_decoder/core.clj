6915b3ca1d7e89590620f98e2712c38176a1866f
admodev <admodevcodes@outlook.com>
2022-02-24 15:52:27 -0300

    add define and set variable action

--- src/boolean_decoder/core.clj
+++ src/boolean_decoder/core.clj
@@ -6,14 +6,15 @@
   (println "Please, enter the value to decode:")
   (let [encoded-val (read-line)]
     (cond
-      (= "1" encoded-val) (println true)
-      (= "0" encoded-val) (println false)
+      (= "1" encoded-val) (def decoded-val true)
+      (= "0" encoded-val) (def decoded-val false)
       (= "" encoded-val) (println "Please, enter a number between (1 or 0).")
       (not= Integer encoded-val) (println "Please, enter a number...")
       :default ""
-      )
     )
   )
+  (println decoded-val)
+)
 
 (defn -main []
   (decode))
